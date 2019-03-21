b262f70e1eccc592bdec794c34f4ea5361a5984a
TRT-MichaelH <michael.humphries@raytheon.co.uk>
2019-03-21 10:55:59 +0000

    adding more urls

--- search-job.py
+++ search-job.py
@@ -1,7 +1,10 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+
 import requests as rq
+import datetime
 
 file_path = 'input/urls.txt'
+compare_string = 'Beautiful'
 
 
 def get_urls(path):
@@ -11,18 +14,46 @@ def get_urls(path):
     return [x.strip() for x in content]
 
 
+def extract_words(html_doc):
+    """returns a list of lines"""
+    # list of each word
+    words = bs(html_doc, 'html.parser').get_text().replace('\n', ' ').strip().split(' ')
+    # lines = bs(html_doc, 'html.parser').get_text().strip().split('\n')
+    # for line in lines:
+    #     line.replace('\n', ' ').replace('\r', '')
+    # words = lines.split('\n')
+    return words
+
+    #return bs(html_doc, 'html.parser').get_text().strip('\n').split(" ")
+
+
+def generate(compare, words, url):
+    sentence = []
+    print('the word to compare: ', compare)
+    for i in range(len(words)):
+        if words[i] == compare:
+            sentence = words[i-5:i+5]
+            url = url
+            return '{} : {}'.format(sentence, url)
+
+
 def process(urls):
-    """gets the raw html data from the request"""
-    data = [rq.get(url).text for url in urls]
-    return data
+    count = 0
+    for url in urls:
+        count += 1
+        url = url
+        raw = rq.get(url).text
+        words = extract_words(raw)
+        statement = generate(compare_string, words, url)
+        print('url {} : {} '.format(count, statement))
+
+    #return '{} : {}'.format(statement[0], statement[1])
 
 
 def run():
+    # returns list of urls
     urls = get_urls(file_path)
-    print(urls)
-    htmls = process(urls)
-    for html in htmls:
-        print(html)
+    process(urls)
 
 
 run()
