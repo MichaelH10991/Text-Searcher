3d45ece18269d9cc372dd5d49714549441fcd7c7
Michael Humphries <michaelhumphries1997@gmail.com>
2019-03-21 15:42:03 +0000

    adding mood detector functionality

--- search-job.py
+++ search-job.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup as bs
+from urllib.parse import urlsplit
 
 import requests as rq
 import datetime
 
 file_path = 'input/urls.txt'
-compare_string = 'crack'
-print('the word to compare {}'.format(compare_string))
+compare_string = 'hate'
+happy_array = ['beautiful', 'cool']
+sad_array = ['hate']
+print('the word to compare: \"{}\"'.format(compare_string))
 
 
 def get_urls(path):
@@ -31,6 +34,24 @@ def generate(compare, words):
     return sentences
 
 
+def mood(happy, sad, words, url):
+    """I dunno if this works lol but it should compute occurences of each word in the array provided"""
+    happy_word_matches = 0
+    sad_word_matches = 0
+
+    for i in range(len(happy)):
+        for word in words:
+            if word == happy[i]:
+                happy_word_matches += 1
+
+    for i in range(len(sad)):
+        for word in words:
+            if word == sad[i]:
+                sad_word_matches += 1
+
+    return [url, happy_word_matches, sad_word_matches]
+
+
 def process(urls):
     """processes each by finding sentences where the word in question appears"""
     for url in urls:
@@ -43,11 +64,30 @@ def process(urls):
         else:
             print('no \"{}\"\'s found in {}'.format(compare_string, url))
 
+        mood_items = mood(happy_array, sad_array, words, url)
+        print('{} has {} matches of happy words and {} matches of sad words'.format(mood_items[0], mood_items[1],
+              mood_items[2]))
+
+        if not mood_items[1] == 0 or not mood_items[2] == 0:
+            happy_ratio = (mood_items[1] + mood_items[2]) / mood_items[1]
+            sad_ratio = (mood_items[1] + mood_items[2]) / mood_items[2]
+            print('happy words {}%'.format(happy_ratio))
+            print('sad words {}%'.format(sad_ratio))
+        elif mood_items[1] > 0:
+            print('happy words 100%')
+        else:
+            print('sad words 100%')
+
+
+
 
 def run():
     # returns list of urls
     urls = get_urls(file_path)
     process(urls)
+
+    # for url in urls:
+    #     print('{}{}'.format(urlsplit(url).netloc, urlsplit(url).path))
 
 
 run()
